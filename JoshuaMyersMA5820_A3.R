7d25c64aa0694ebf5fe1458092a802b00c9ab75d
Josh-Myers <myers.josh@gmail.com>
2019-11-21 15:41:51 +1000

    first draft

--- JoshuaMyersMA5820_A3.R
+++ JoshuaMyersMA5820_A3.R
@@ -1,7 +1,8 @@
-# assessment 3: R code
+# Joshua Myers MA5820 assessment 3: R code
 
 # import packages----
 library(tidyverse)
+library(cowplot)
 
 # question 1----
 # The lifetime of a particular type of TV follows a normal distribution: mean = 4800 hours, and standard deviation (sd) = 400 hours.
@@ -31,66 +32,70 @@ print(prob_y)
 # measured for each person pre- and post exercise. Using this sample, test if Beta endorphins increase with exercise. Adopt a 5% risk of 
 # committing a type I error.
 
-# a. read data into R
+# read data into R
 # data is in a csv file called "beta.csv", which I have put in my working directory
 beta_df = read.csv('beta.csv')
 # check that data has loaded correctly, and get overview of data types, which are all numeric
+View(beta_df)
 str(beta_df)
 
-# b. exploratory data analysis
+# exploratory data analysis
 # density plot of dif 
 beta_density = beta_df %>% 
   ggplot(aes(x = dif)) +
   geom_density(colour='lightblue', fill='lightblue') +
   scale_x_continuous(limits = c(0, 100), labels = c(0, 25, 50, 75, 100)) 
-# pre looks fairly normal, and post also apart from extreme outlier
-# variance for post looks higher than pre, however
+# there is an extreme outlier, but relatively normal apart from this
 
 # qq plot
 beta_qq = beta_df %>% 
   ggplot(aes(sample=dif)) +
   stat_qq() +
   stat_qq_line()
-# there is an extreme outlier 
 # there is one extreme outlier, however, I do not wish to remove it because it may be a genuine result
-# therefore, I will analyse with non-parametric method
-# Wilcoxon matched-pairs signed-ranks test, to test if there is a statistically significant difference between pre and post
-wilcox.test(beta_df$dif, alternative = "greater") # greater because we hypothesise diff is positive (post - pre)
-# strong evidence of an effect
 
-# question 3
-plant_df = PlantGrowth
-summary(plant_df)
+# save plots
+beta_plots = plot_grid(beta_density, beta_qq, nrow = 1, labels = c("A", "B"))
+save_plot('beta_plots.png', beta_plots, base_aspect_ratio = 2, base_height=5, base_width=NULL)
 
-plant_density = plant_df %>% 
-  ggplot(aes(x = weight, colour=group, fill=group)) +
-  geom_density(alpha=0.5) 
+# statistical testing
+# I will use a non-parametric method
+w_test = wilcox.test(beta_df$dif, mu=0, alternative = "greater") 
+print(w_test)
+# mu is H0 median
+# greater because we hypothesise diff is positive
+# there strong evidence that the median diff is not 0
 
-# qq plot
-plant_qq = plant_df %>% 
-  ggplot(aes(sample=weight, colour=group)) +
-  stat_qq() +
-  stat_qq_line()
+# question 3----
+# The dataset PlantGrowth is a datafile in the R datasets. If you type `PlantGrowth' from the R prompt you will see the data. For more 
+# information about this data type help (PlantGrowth) from the R prompt. These data are obtained from an experiment to compare plant yields
+# under a control and two other treatments. Test if there is a difference between the control group and treatment 2 on mean plant yields.
+# assign dataset to a variable and examine
+data(PlantGrowth)
+View(PlantGrowth)
+str(PlantGrowth)
 
 # box plot 
-plant_box = plant_df %>% 
+plant_box = PlantGrowth %>% 
   ggplot(aes(x = group, y = weight, fill=group, colour=group)) +
   geom_jitter(width=0.16) +
   geom_boxplot(colour='black', alpha=0.4) 
 
-bartlett.test(weight~group, plant_df) # not significant - no evidence of unequal variance
-shapiro.test(plant_df$weight)
+# qqplot plot
+plant_qq = PlantGrowth %>% 
+  ggplot(aes(sample=weight, colour=group)) +
+  stat_qq() +
+  stat_qq_line()
+# looks approximately normal, there are a couple of outliers in trt1, but we are not interested in that group
 
-f = lm(weight ~ group, data = plant_df)
-anova(f)
-summary(f) # The interpretation of the estimates is that the intercept is the mean in the first group (ctrl), 
-# whereas the two others describe the difference between that group and the first one.
+# save plots
+plant_plots = plot_grid(plant_box, plant_qq, nrow = 1, labels = c("A", "B"))
+save_plot('plant_plots.png', plant_plots, base_aspect_ratio = 2, base_height=6, base_width=NULL)
 
-an = aov(weight ~ group, data = plant_df)
-summary(an)
+# remove trt1 for this test
+plant_df = PlantGrowth %>% 
+  filter(group != 'trt1')
 
-pairwise.t.test(plant_df$weight, plant_df$group, p.adj = "bonf")
-# evidence that mean trt2 is different trt1
-# no evidence that mean trt2 is diff ctrl or ctrl is diff trt1
-
+t_test = t.test(weight~group, plant_df)
+print(t_test)
 
